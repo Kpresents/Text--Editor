87addb6481bcddae39da9f56c0bae6e188d98621
Carolina Hernandez <kpresents.0@gmail.com>
2022-09-24 18:05:02 -0700

    updated folder structure and updated scr database.js, install.js

---
+++ client/src/js/database.js
@@ -0,0 +1,44 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {console.log('PUT DB  implemented');
+const jateDb = await openDB('jate', 1);
+const tx= jateDb.transaction ('jate', 'readwrite');
+const request = store.put ({ content: content});
+const store = tx.objectStore('jate');
+const result= await request;
+return result;
+}
+
+
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+console.log('GET DB implemented');
+const jateDb = await openDB('jate', 1);
+const tx= jateDb.transaction ('jate', 'readonly');
+const request = store.getAll ();
+const store = tx.objectStore('jate');
+const result= await request;
+console.log ('result.value', result)
+return result;
+
+
+}
+
+
+
+
+initdb();
