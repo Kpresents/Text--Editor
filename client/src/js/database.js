8eac64afe716add8ab9cbaa6736a9881c2fa1971
Carolina Hernandez <kpresents.0@gmail.com>
2022-09-26 21:08:04 -0700

    launching on heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,8 +16,9 @@ const initdb = async () =>
 export const putDb = async (content) => {console.log('PUT DB  implemented');
 const jateDb = await openDB('jate', 1);
 const tx= jateDb.transaction ('jate', 'readwrite');
-const request = store.put ({ content: content});
+
 const store = tx.objectStore('jate');
+const request = store.add ({content: content});
 const result= await request;
 return result;
 }
@@ -29,8 +30,9 @@ export const getDb = async () => {
 console.log('GET DB implemented');
 const jateDb = await openDB('jate', 1);
 const tx= jateDb.transaction ('jate', 'readonly');
-const request = store.getAll ();
+
 const store = tx.objectStore('jate');
+const request = store.getAll();
 const result= await request;
 console.log ('result.value', result)
 return result;
